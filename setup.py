43b4a4e49f494fd0473126b18563a2bd8ad62965
Maikel Nadolski <maikel.nadolski@gmail.com>
2016-01-02 10:42:32 +0100

    Test commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup (
+       name='surveillance',
+       version='0.1',
+       packages=find_packages(),
+
+       # Declare your packages' dependencies here, for eg:
+       install_requires=['cv2>=3'],
+
+       # Fill in these to make your Egg ready for upload to
+       # PyPI
+       author='Maikel Nadolski',
+       author_email='maikel.nadolski@googlemail.com',
+
+       #summary = 'Just another Python package for the cheese shop',
+       url='',
+       license='MIT',
+       long_description='Various Scripts to control the camera in a car.',
+
+       # could also include long_description, download_url, classifiers, etc.
+
+  
+       )
